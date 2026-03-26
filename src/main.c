59069ea04697c249cbfb41c8bb7d47298dd64294
creeper6530 <o.behal@centrum.cz>
2026-03-26 21:58:44 +0100

    WIP: Added a bit more functions, but not much new

--- src/main.c
+++ src/main.c
@@ -1,16 +1,22 @@
 #include <ncurses.h>
+#include <stdio.h>
 
 //#include "lib.h"
 
 // https://tldp.org/HOWTO/NCURSES-Programming-HOWTO/helloworld.html
 int main(void) {
-    initscr(); // Init the ncurses system; init the terminal into curses mode. 
+    initscr(); // Init the ncurses system; init the terminal into curses mode. Just returns a pointer to stdscr
+    cbreak(); // Disable line buffering; remove character processing (except for interrupts like Ctrl-C)
+    keypad(stdscr, TRUE); // Enable parsing escape sequences into function keys, arrow keys and similar
+    noecho(); // Don't echo user input to the screen
     
     printw("Hello, world! Press any key to exit."); // Print the message at the current coords - default (y,x) = (0,0)
     refresh(); // Flush the internal buffer to the display
 
-    getch(); // Get character - wait for user input
+    int ch = getch(); // Get character - wait for user input
 
-    endwin(); // Deinit ncurses; switch back to normal terminal
+    endwin(); // Switch back to normal terminal (calling refresh() or doupdate() after this would resume curses mode)
+    delwin(stdscr); // Free the memory allocated for the window
+    fprintf(stderr, "Exiting...\n"); // Just test a clean exit
     return 0;
 }
