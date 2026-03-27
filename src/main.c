5e3698290b3bf8ece138b6bd2487051f1406801f
creeper6530 <o.behal@centrum.cz>
2026-03-27 19:01:09 +0100

    Fix the resize by just repainting the UI anew

--- src/main.c
+++ src/main.c
@@ -3,8 +3,14 @@
 
 #include <stdio.h>
 
+struct RepaintReturn {
+    WINDOW * central_win;
+    int size_y;
+    int size_x;
+};
+
 WINDOW * draw_central_window(int win_size_y, int win_size_x);
-void recentre_resize_window(WINDOW * win_ptr, int win_size_y, int win_size_x);
+struct RepaintReturn repaint_all(WINDOW * central_win);
 
 // https://tldp.org/HOWTO/NCURSES-Programming-HOWTO/helloworld.html
 // https://github.com/mcdaniel/curses_tutorial
@@ -14,16 +20,10 @@ int main(void) {
     keypad(stdscr, true); // Enable parsing escape sequences into function keys, arrow keys and similar
     noecho(); // Don't echo user input to the screen
 
-    // Print the message at the current coords - default (y,x) = (0,0)
-    printw("Hello, world! Press a non-letter key to exit.");
-    refresh(); // Flush the internal buffer to the display
-
-    int size_y, size_x; // You can declare multiple vars of same type in one go in C
-    getmaxyx(stdscr, size_y, size_x);
-    mvprintw(1, 0, "Window size: %ix%i", size_y, size_x);
-
-    // Leave 5 chars from each side for the X coordinate
-    auto central_win = draw_central_window(5, -10);
+    struct RepaintReturn repaint_data = repaint_all(nullptr);
+    int size_y = repaint_data.size_y;
+    int size_x = repaint_data.size_x;
+    WINDOW * central_win = repaint_data.central_win;
 
     mmask_t newmask = BUTTON1_CLICKED;
     mousemask(newmask, nullptr); // Don't save old mouse mask
@@ -34,15 +34,19 @@ int main(void) {
 
         switch (ch) {
             case KEY_RESIZE:
-                endwin();
-                fprintf(stderr, "Resizing the window is not currently supported. Please do not the cat.\n");
-                return 1;
+                repaint_data = repaint_all(central_win);
+                size_y = repaint_data.size_y;
+                size_x = repaint_data.size_x;
+                central_win = repaint_data.central_win;
+                break;
 
             case KEY_MOUSE:
                 MEVENT event;
                 if (getmouse(&event) == OK) {
                     if (event.bstate & BUTTON1_CLICKED) {
-                        mvprintw(4, 0, "Mouse clicked at: %i %i", event.x, event.y);
+                        move(3, 0);
+                        clrtoeol();
+                        printw("Mouse clicked at: %i %i", event.x, event.y);
                         refresh();
                     }
                 }
@@ -95,26 +99,26 @@ WINDOW * draw_central_window(int win_size_y, int win_size_x) {
     return win_ptr;
 }
 
-// TODO: Appears to be severely broken when window width changes
-// Some more sporadic bugs when changing just heigth too
-void recentre_resize_window(WINDOW * win_ptr, int win_size_y, int win_size_x) {
-    int size_y, size_x;
+struct RepaintReturn repaint_all(WINDOW * central_win) {
+    if (central_win != nullptr)
+        delwin(central_win);
+
+    clear();
+    // Print the message at the current coords - default (y,x) = (0,0)
+    mvprintw(0, 0, "Hello, world! Press a non-letter key to exit.");
+    refresh(); // Flush the internal buffer to the display
+
+    int size_y, size_x; // You can declare multiple vars of same type in one go in C
     getmaxyx(stdscr, size_y, size_x);
+    mvprintw(1, 0, "Window size: %ix%i", size_y, size_x);
 
-    if (win_size_y < 0)
-        win_size_y = size_y + win_size_y;
-    if (win_size_x < 0)
-        win_size_x = size_x + win_size_x;
+    // Leave 5 chars from each side for the X coordinate
+    central_win = draw_central_window(5, -10);
+    refresh();
 
-    int win_topleft_y = (size_y - win_size_y) / 2;
-    int win_topleft_x = (size_x - win_size_x) / 2;
-
-    wclear(win_ptr);
-    wrefresh(win_ptr);
-
-    wresize(win_ptr, win_size_y, win_size_x);
-    mvwin(win_ptr, win_topleft_y, win_topleft_x);
-
-    box(win_ptr, 0, 0);
-    wrefresh(win_ptr);
+    return (struct RepaintReturn) {
+        .central_win = central_win,
+        .size_y = size_y,
+        .size_x = size_x
+    };
 }
