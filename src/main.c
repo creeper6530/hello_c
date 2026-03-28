d6c1fde0d864f206c11901324b9d2fe97ac53875
creeper6530 <o.behal@centrum.cz>
2026-03-28 16:09:40 +0100

    Leave central window at default colours (e.g. for light-mode terminals); move the window pointers into static globals (=can't be used outside this translation unit)

--- src/main.c
+++ src/main.c
@@ -4,17 +4,11 @@
 #include <assert.h>
 #include <stdio.h>
 
-// Need to wrap the array in a struct to be able to pass it as a parameter
-struct RepaintArray {
-    WINDOW * winptrs[5]; // Support up to 5 window pointers
-};
-struct RepaintReturn {
-    WINDOW * winptrs[5];
-    int size_y;
-    int size_x;
-};
+void repaint_all(void);
 
-struct RepaintReturn repaint_all(struct RepaintArray);
+// Globals
+static WINDOW * central_win = nullptr;
+static WINDOW * controls_win = nullptr;
 
 // https://tldp.org/HOWTO/NCURSES-Programming-HOWTO/helloworld.html
 // https://github.com/mcdaniel/curses_tutorial
@@ -27,18 +21,11 @@ int main(void) {
     if (has_colors()) {
         start_color(); // Enable color functionality
         use_default_colors(); // Allow using the terminal's default background color (instead of black)
-
-        init_pair(1, COLOR_WHITE, -1); // Define color pair 1 as white foreground and default background
+        
         init_pair(2, COLOR_BLACK, COLOR_CYAN); // Define color pair 2 as black foreground, cyan background
     }
 
-    struct RepaintReturn repaint_data = repaint_all(
-        (struct RepaintArray) {} // Leave all null
-    );
-    int size_y = repaint_data.size_y;
-    int size_x = repaint_data.size_x;
-    WINDOW * central_win = repaint_data.winptrs[0];
-    WINDOW * controls_win = repaint_data.winptrs[1];
+    repaint_all();
 
     /*mmask_t newmask = BUTTON1_CLICKED;
     mousemask(newmask, nullptr); // Don't save old mouse mask*/
@@ -49,15 +36,7 @@ int main(void) {
 
         switch (ch) {
             case KEY_RESIZE:
-                repaint_data = repaint_all(
-                    (struct RepaintArray) {
-                        .winptrs = {central_win, controls_win} // Leave the rest null
-                    }
-                );
-                size_y = repaint_data.size_y;
-                size_x = repaint_data.size_x;
-                central_win = repaint_data.winptrs[0];
-                controls_win = repaint_data.winptrs[1];
+                repaint_all();
                 break;
 
             /*case KEY_MOUSE:
@@ -89,11 +68,14 @@ int main(void) {
 }
 
 // Supports up to 5 windows, but for now we only use two.
-struct RepaintReturn repaint_all(struct RepaintArray wrapper) {
-    for (int i = 0; i < 5; i++) {
-        if (wrapper.winptrs[i] != nullptr) {
-            delwin(wrapper.winptrs[i]);
-        }
+void repaint_all(void) {
+    if (central_win != nullptr) {
+        delwin(central_win);
+        central_win = nullptr; // Destroy the old dangling pointer
+    }
+    if (controls_win != nullptr) {
+        delwin(controls_win);
+        controls_win = nullptr;
     }
 
     standend(); // Clear all attributes
@@ -105,8 +87,8 @@ struct RepaintReturn repaint_all(struct RepaintArray wrapper) {
     // ------------------------------
 
     // size_y - 1 = 1 line from bottom (last line)
-    WINDOW * controls_win = newwin(1, size_x, size_y - 1, 0); // nlines, ncols, begin_y, begin_x
-    assert(controls_win != nullptr);
+    controls_win = newwin(1, size_x, size_y - 1, 0); // nlines, ncols, begin_y, begin_x
+    assert(controls_win != nullptr); // We do not handle the error.
 
     if (has_colors()) {
         wattr_on(controls_win, COLOR_PAIR(2), nullptr);
@@ -124,12 +106,8 @@ struct RepaintReturn repaint_all(struct RepaintArray wrapper) {
     // ------------------------------
 
     // Leave 1 line at the bottom for controls
-    WINDOW * central_win = newwin(size_y - 1, size_x, 0, 0); // nlines, ncols, begin_y, begin_x
+    central_win = newwin(size_y - 1, size_x, 0, 0); // nlines, ncols, begin_y, begin_x
     assert(central_win != nullptr);
-
-    if (has_colors()) {
-        wattr_on(central_win, COLOR_PAIR(1), nullptr);
-    }
 
     box(central_win, 0, 0); // Draw a box around the central window with default line drawing characters
     int greet_start_x = (size_x / 2) - 17; // The greeting message is about 32 chars, so we start it 16 chars before the middle to center it
@@ -138,12 +116,4 @@ struct RepaintReturn repaint_all(struct RepaintArray wrapper) {
     mvwprintw(central_win, 1, 2, ">>> ");
 
     wrefresh(central_win);
-    
-    // ------------------------------
-
-    return (struct RepaintReturn) {
-        .winptrs = {central_win, controls_win},
-        .size_y = size_y,
-        .size_x = size_x
-    };
 }
